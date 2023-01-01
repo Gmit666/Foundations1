0b918a8a0d8eea38e47a8ac744e14150cc21408f
RonGord <121050357+RonGord@users.noreply.github.com>
2023-01-01 17:14:22 +0200

    Update airport_manager.h

--- FProject/headers/airport_manager.h
+++ FProject/headers/airport_manager.h
@@ -17,6 +17,7 @@ int      initAirportManager(AirportManager* pAirportManager);
 void     printAirportManager(const AirportManager* pAirportManager);
 void     freeAirportManager(AirportManager* pAirportManager);
 
+int checkContry(char* name);
 int      addAirport(AirportManager* pAirportManager);
 Airport* findAirportByIATA(const AirportManager* pAirportManager, const char code[IATA_CODE_SIZE + 1]);
 
